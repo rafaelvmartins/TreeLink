6eecb0f83d0d82060a0148854f868ef03ddc538e
Rafael Martins <rafaelvianamartins@gmail.com>
2024-04-16 23:41:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains('light')) {
+    //se estiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    //se tiver sem o lght mode, manter a imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+  }
+}
+/*
+    html.classList.remove('light')
+ 
+    html.classList.add('light')
+  }
+}
+*/
